6a9f04c65e812375e897d7579b9c912c7755f17a
MieszkoN <mieszkoniewiarowski@wp.pl>
2022-03-08 01:38:30 +0100

    Add changes for newton's method

--- main.py
+++ main.py
@@ -25,12 +25,22 @@ def main():
             (a, b, c, d, x) = menu.provide_coefficients_fx()
             menu.clear_screen()
             if method_choice == 1:
-                print(gradient_descent.execute_fx(a, b, c, d))
+                print(gradient_descent.execute_fx(a, b, c, d, x, stopping_condition, stopping_value))
             elif method_choice == 2:
-                print(newton.execute_fx(a, b, c, d, x, stopping_condition, stopping_value))
+                restart_method = menu.choose_restart_method()
+                repetitions = menu.provide_restart_repetitions(restart_method)
+                for _ in range(repetitions):
+                    print(newton.execute_fx(a, b, c, d, x, stopping_condition, stopping_value))
         elif function_choice == 2:
-            menu.provide_coefficients_gx()
-
+            (a, b, c, d, x) = menu.provide_coefficients_gx()
+            menu.clear_screen()
+            if method_choice == 1:
+                print(gradient_descent.execute_gx(a, b, c, d, x, stopping_condition, stopping_value))
+            elif method_choice == 2:
+                restart_method = menu.choose_restart_method()
+                repetitions = menu.provide_restart_repetitions(restart_method)
+                for _ in range(repetitions):
+                    print(newton.execute_gx(a, b, c, d, x, stopping_condition, stopping_value))
         
             
 if __name__ == '__main__':
