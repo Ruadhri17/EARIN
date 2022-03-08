9f03fd7febe1125fab334a8063833731458e4be8
ruadhri17 <K.R.Piotrowski17@gmail.com>
2022-03-08 12:20:15 +0100

    added gx for newton

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-# import numpy as np
+import numpy as np
 
 def f_original(x, a, b, c, d):
     return a * pow(x, 3) + b * pow(x, 2) + c * x + d
@@ -11,8 +11,12 @@ def f(x, a, b, c):
 def f_prime(x, a, b):
     return 6 * a * x + 2 * b
 
-def g(x, A, b, c, d):
-    return #write function
+# c + b'x + x'Ax
+def g(x, A, b, c):
+    return np.add(np.add(c, np.add(np.transpose(b), x)), np.dot(np.dot(np.transpose(x), A), x))
+# 
+def g_first_gradient(x, A, b, c):
+    return np.gradient(g(x, A, b, c))
 
-def g_prime(x, a, b, c):
-    return #write function
+def g_second_gradient(x, A, b, c):
+    return np.gradient(g_first_gradient(x, A, b, c))
--- newton.py
+++ newton.py
@@ -18,15 +18,15 @@ def execute_fx(a: float, b: float, c: float, d: float, x0: float, stopping_condi
 
 def execute_gx(A, b: float, c: float, d: float, x0: float, stopping_condition: int, stopping_value):
     xn = x0
-    # if stopping_condition == 1: 
-    #     for _ in range(stopping_value):
-    #         # xn = xn - functions.f(xn, A, b, c, d) / functions.f_prime(xn, A, b, c)
-    # elif stopping_condition == 2:
-    #     while abs(xn) > stopping_value:
-    #         # xn = xn - functions.f(xn, a, b, c, d) / functions.f_prime(xn, a, b, c)   
-    # elif stopping_condition == 3:
-    #     t_end = time.time() + stopping_value
-    #     print("Please wait for a while...")
-    #     while time.time() < t_end:
-    #         xn = xn - functions.f(xn, A, b, c, d) / functions.f_prime(xn, A, b, c)   
-    # return (xn, functions.f(xn, A, b, c, d))
+    if stopping_condition == 1:
+        for _ in range(stopping_value):
+            xn = xn - functions.g_first_gradient / functions.g_second_gradient
+    elif stopping_condition == 2:
+        while abs(xn) > stopping_value:
+            xn = xn - functions.g_first_gradient / functions.g_second_gradient
+    elif stopping_condition == 3:
+         t_end = time.time() + stopping_value
+         print("Please wait for a while...")
+         while time.time() < t_end:
+             xn = xn - functions.g_first_gradient / functions.g_second_gradient
+    return (xn, functions.g(xn, A, b, c, d))
