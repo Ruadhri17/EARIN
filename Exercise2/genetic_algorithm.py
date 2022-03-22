6da815d0ea41a6b42823e1567d8c27ba2ed12f69
ruadhri17 <K.R.Piotrowski17@gmail.com>
2022-03-23 00:49:45 +0100

    finished

--- Exercise2/genetic_algorithm.py
+++ Exercise2/genetic_algorithm.py
@@ -1,10 +1,8 @@
-import string
 import numpy as np
 from random import uniform, randint
 
 def fx(x, A, b, c):
     return np.dot(np.dot(np.transpose(x), A), x)  + np.dot(np.transpose(b), x) + c
-
 
 def convert_binary_to_int(genome):
     number_vector = []
@@ -39,12 +37,15 @@ def roulette_wheel_selection(population, A, b, c):
     population.sort(reverse=True, key= lambda g: fitness(g, A, b, c))
     # Sum fitnesses
     for genome in population:
-        sum_of_fitnesses += fitness(genome, A, b, c)
+        edited_fitness = fitness(genome, A, b, c) + 2*abs(fitness(population[-1], A, b, c)) 
+        sum_of_fitnesses += edited_fitness
     
-    desired_value = uniform(0, sum_of_fitnesses)
+    desired_value = uniform(0.0, 1.0)
+    population.sort(reverse=False, key= lambda g: fitness(g, A, b, c))
     # Find parent
     for genome in population:
-        partial_sum += fitness(genome, A, b, c)
+        edited_fitness = (fitness(genome, A, b, c) + 2*abs(fitness(population[0], A, b, c))) / sum_of_fitnesses
+        partial_sum += edited_fitness
         if partial_sum > desired_value:
             return genome
             
@@ -54,7 +55,7 @@ def crossover(first_parent, second_parent, crossover_probability):
     # Choose randomly crossover point
     crossover_point = randint(1, first_parent[0].size)
     # Determine if crossover will happen
-    if uniform(0.0 , 1.0) > crossover_probability:
+    if uniform(0.0, 1.0) > crossover_probability:
         return (first_parent, second_parent)
     # prepare children 
     first_child = []
@@ -66,21 +67,50 @@ def crossover(first_parent, second_parent, crossover_probability):
 
     return (first_child, second_child)
 
+def mutation(child, mutation_probability):
+    for binary_number in child:
+         with np.nditer(binary_number, op_flags=['readwrite']) as itr:
+            for digit in itr:
+                if uniform(0.0, 1.0) < mutation_probability:
+                    digit[...] = 1 if digit == 0 else 0
+    return child
+
+def print_results(population, A, b, c, gen_nr):
+    population.sort(reverse=True, key= lambda g: fitness(g, A, b, c))
+    achieved_x = convert_binary_to_int(population[0])
+    achieved_fx = fx(achieved_x, A, b, c)
+    d = {}
+    d[str(achieved_x)] = achieved_fx
+    for v in d.items():
+        x_value, fun_value = v
+        print("{:<6} {:<10} {:<6}".format(gen_nr, x_value, fun_value))
+
 def genetic_algorithm(A, b, c, d, n, population_size, mutation_probability, crossover_probability, number_of_iterations):
     population = create_population(population_size, n, d)
-    
-    for _ in range(number_of_iterations):
-        next_generation = []
-        for _ in range(int(len(population)/2) - 1):
-            # Choose parent (roulette wheel selection) 
-            first_parent = roulette_wheel_selection(population, A, b, c)
-            second_parent = roulette_wheel_selection(population, A, b, c)
-            # Cross parents
-            (first_child, second_child) = crossover(first_parent, second_parent, crossover_probability)
-            # After Crossing preserve child for next generations
-            next_generation.append(first_child)
-            next_generation.append(second_child)
-            
-    return next_generation
+    print("{:<6} {:<10} {:<6}".format('Gen.', 'X value', 'Fun. value'))
+    for gen_nr in range(number_of_iterations):
+        # Choose parent (roulette wheel selection) 
+        first_parent = roulette_wheel_selection(population, A, b, c)
+        second_parent = roulette_wheel_selection(population, A, b, c)
+        
+        # Cross parents
+        (first_child, second_child) = crossover(first_parent, second_parent, crossover_probability)
+        
+        # Mutation 
+        first_child_after_mutation = mutation(first_child, mutation_probability)
+        second_child_after_mutation = mutation(second_child, mutation_probability)
+        
+        # FIFO Replacement
+        population.pop(0)
+        population.pop(0)
+        population.append(first_child_after_mutation)
+        population.append(second_child_after_mutation)
+        if gen_nr == 0:
+            print_results(population, A, b, c, gen_nr+1)
+        elif gen_nr % 10 == 9:
+            print_results(population, A, b, c, gen_nr+1)
 
-# [[array(1,0,1), array(1,1,0)], [array(1,1,1), array(0,1,0)]]
+    print("--------------------------------------------------------------")
+    print_results(population, A, b, c, gen_nr+1)   
+    
+    return population
--- Exercise2/menu_service.py
+++ Exercise2/menu_service.py
@@ -7,5 +7,5 @@ def run_fx():
         (searched_integers_range_power, population_size, crossover_probability, mutation_probability, algorithm_iterations) = menu.provide_algorithm_parameters()
         (A, b, c, problem_dimensionality) = menu.provide_coefficients_fx()
         genetic_algorithm.genetic_algorithm(A, b, c, searched_integers_range_power, problem_dimensionality, population_size, mutation_probability, crossover_probability, algorithm_iterations)
-        if menu.finish_program():
+        if menu.finish_program() == "y" or menu.finish_program == "yes":
             exit(0)
