f8244b4355661f8ed4792fc83c3e8d17b1c5585d
ruadhri17 <K.R.Piotrowski17@gmail.com>
2022-04-03 17:56:27 +0200

    small text fixes

--- Exercise3/main.py
+++ Exercise3/main.py
@@ -1,7 +1,20 @@
 from tictactoe import play
 
-def main(): 
-    play()
-    
 if __name__ == '__main__':
-    main()
+    play()
+
+
+"""
+TicTacToe: console application
+Instruction: 
+1. Run python program using command "python3 main.py".
+2. Choose the mark which you want to have during gameplay by typing 'x' or 'o'(uppercase also possible)
+3. Choose whether you want to start by typing 'y'(uppercase also possible). 
+   If you typed 'n'(uppercase also possible) the AI player will start
+4. Choose the position inside the board for your mark. In order to do it 
+   you have to provide number from 1 to 9:
+   - 1 to 3 for the top row
+   - 4 to 6 for the middle row
+   - 7 to 9 for the bottom row
+5. Enjoy your game!
+"""
