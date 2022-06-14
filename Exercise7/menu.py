655eeebc3c10e44a095bf838c6bff83777b3246e
ruadhri17 <K.R.Piotrowski17@gmail.com>
2022-06-14 22:05:48 +0200

    small fixes

--- Exercise7/menu.py
+++ Exercise7/menu.py
@@ -1,4 +1,4 @@
-from constants import LINE, MCMC, MARKOV_BLANKET, FILENAME, YES, PROVIDE_NODE
+from constants import LINE, MCMC, MARKOV_BLANKET, YES, PROVIDE_NODE, INCORRECT_DEFINED_DATA, NEW_VARIABLES
 from mcmc import set_parameters
 from text_handler import print_error
 from bayesian_network import BayesianNetwork
@@ -13,8 +13,7 @@ def run_menu():
             steps = ''
 
             option_enum = [MCMC, MARKOV_BLANKET]
-            option = int(
-                input('- Choose the method by typing 0 ({}) or 1 ({})  : '.format(option_enum[0], option_enum[1])))
+            option = int(input('- Choose the method by typing 0 ({}) or 1 ({})  : '.format(option_enum[0], option_enum[1])))
 
             if option == 0:
                 evidence, query, steps = set_parameters()
@@ -26,25 +25,24 @@ def run_menu():
             print(LINE)
             execute_method(evidence, query, steps, option_enum[option])
 
-            ans = 'y'
-            while ans in YES:
-                print(LINE)
-                ans = input('- Do you want to run {} method once again (y/n): '.format(option_enum[option]))
-                if ans in YES:
+            if option == 0:
+                ans = 'y'
+                while ans in YES:
                     print(LINE)
-                    execute_method(evidence, query, steps, option_enum[option])
+                    ans = input('- Do you want to run {} method once again (y/n): '.format(option_enum[option]))
+                    if ans in YES:
+                        print(LINE)
+                        execute_method(evidence, query, steps, option_enum[option])
 
-            ans = input('- Do you want to provide new variables (y/n): ')
+            ans = input(NEW_VARIABLES)
             print(LINE)
             if ans not in YES:
                 sys.exit()
 
         except ValueError:
-            error = 'Defined input variables are incorrect! Please define valid one!'
-            print_error(error)
+            print_error(INCORRECT_DEFINED_DATA)
         except AttributeError:
-            error = 'Defined input variables are incorrect! Please define valid one!'
-            print_error(error)
+            print_error(INCORRECT_DEFINED_DATA)
 
 def execute_method(evidence, query, steps, option):
     bayes_net = BayesianNetwork()
